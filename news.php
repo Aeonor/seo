5a639467cb4cffa9270e8479aefc0b0f92b0939e
mcastanheiro <mcastanheiro@BUSINESS-860935.BETD.FR>
2013-04-26 14:59:59 +0200

    homepage + content homepage

---
+++ news.php
@@ -0,0 +1,4 @@
+<?php 
+$_GET['page'] = 'news';
+require 'init.php';
+
---
+++ src/news.php
@@ -0,0 +1,10 @@
+<div class="row members">  
+  <div class="span3 offset2">
+    <h2 data-content-name="congres/annuel_label"><?php echo getData($_PAGE, $_SESSION['lng'], 'annuel_label', 'Congrès annuel') ?></h2>
+    <div class="congres_content" data-content-name="congres/annuel_content"><?php echo getData($_PAGE, $_SESSION['lng'], 'annuel_content', '') ?></div>
+  </div>
+  <div class="span3 offset2">
+    <h2 data-content-name="congres/congres_future"><?php echo getData($_PAGE, $_SESSION['lng'], 'congres_future', 'Congrès annuel') ?></h2>
+    <div class="congres_content" data-content-name="congres/future_content"><?php echo getData($_PAGE, $_SESSION['lng'], 'future_content', '') ?></div>
+  </div>
+</div>
